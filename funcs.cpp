533597e506e603222e3cdeb1a20df272d13cf76f
Michelle <Michelle@Johns-MBP-3.home>
2022-10-07 19:08:17 -0400

    Main File and Funcs

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,102 @@
+/*
+Author: Michelle Wen
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 5
+
+Functions with prime number
+*/
+
 #include <iostream>
 #include "funcs.h"
 
-// add functions here
+bool isDivisibleBy(int n, int d)
+{
+	if(n%d==0)
+	{
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool isPrime(int n)
+{
+	int num=0;
+	for(int i=1; i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			num++;
+		}
+	}
+	if(num > 2)
+	{
+		return false;
+	} else {
+		return true;
+	}
+	
+}
+
+int nextPrime(int n)
+{
+	n++;
+	while(!isPrime(n))
+	{
+		n++;
+	}
+	return n;
+}
+
+int countPrimes(int a, int b)
+{
+	int num=0;
+	for(int i=a; i<=b;i++)
+	{
+		if(isPrime(i))
+		{
+			num++;
+		}
+	}
+	return num;
+}
+
+bool isTwinPrime(int n)
+{
+	if(isPrime(n) && (isPrime(n-2) || isPrime(n+2)))
+	{
+		return true;
+	} else {
+		return false;
+	}
+}
+
+int nextTwinPrime(int n)
+{
+	n+=2;
+	while(!isTwinPrime(n))
+	{
+		n++;
+	}
+	return n;
+}
+
+int largestTwinPrime(int a, int b)
+{
+	while(!isTwinPrime(b) && b>a)
+        {
+             	b--;
+        }
+	if(isTwinPrime(b))
+	{
+		return b;
+	} else {
+		return -1;
+	}
+}
+
+
+
+
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,76 @@
+/*
+Author: Michelle Wen
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 5
+
+Main to test all functions.
+*/
+
 #include <iostream>
-
-
 #include "funcs.h"
 
 int main()
 {
-  return 0;
+	std::cout << "isDivisibleBy(10,5);\n"; 
+	if(isDivisibleBy(10,5)) {
+		std::cout << "Yes \n";
+	} else {
+		std::cout << "No \n";
+	}
+	std::cout << "isDivisibleBy(10,3);\n";
+        if(isDivisibleBy(10,3)) {
+                std::cout << "Yes \n";
+        } else {
+                std::cout << "No \n";
+        }
+	std::cout << "\n------------------\n";
+
+	std::cout << "isPrime(7);\n";
+	if(isPrime(7)) {
+		std::cout << "Yes \n";
+	} else {
+		std::cout << "No \n";
+	}
+	std::cout << "isPrime(6);\n";
+        if(isPrime(6)) {
+                std::cout << "Yes \n";
+        } else {
+                std::cout << "No \n";
+        }
+	std::cout << "\n------------------\n";
+
+	std::cout << "nextPrime(3);\n";
+        std::cout << nextPrime(3);
+        std::cout << "\n------------------\n";
+
+	std::cout << "countPrimes(1,11);\n";
+	std::cout << countPrimes(1,11);
+        std::cout << "\n------------------\n";
+
+	std::cout << "isTwinPrime(3);\n";
+	if(isTwinPrime(3)) {
+                std::cout << "Yes \n";
+        } else {
+                std::cout << "No \n";
+        }
+	std::cout << "isTwinPrime(23);\n";
+        if(isTwinPrime(23)) {
+                std::cout << "Yes \n";
+        } else {
+                std::cout << "No \n";
+        }
+        std::cout << "\n------------------\n";
+
+	std::cout << "nextTwinPrime(19);\n";
+	std::cout << nextTwinPrime(19);
+        std::cout << "\n------------------\n";
+
+	std::cout << "largestTwinPrime(1,18);\n";
+	std::cout << largestTwinPrime(1,18); 
+	std::cout << "\nlargestTwinPrime(20,25);\n";
+        std::cout << largestTwinPrime(20,25);
+        std::cout << "\n------------------\n";
+	
+	return 0;
 }
